4e4f62139f9433b104ca1bcd58f362f4a147b5d2
tsipokoraisha-glitch <tsipokoraisha@gmail.com>
2026-05-31 17:39:39 +0000

    initials to my name

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# initials.py
+# Description: Print my initials using large block letters
+# ASSIGNMENT NUMBER: 1
+
+# NAME: Aisha Mayor Tsipokor
+# STUDENT ID: 2425403971
+# EMAIL: 2425403971@live.gctu.edu.gh
+# GRADER: Augustus Buckman
+# On my honor , Aisha Mayor Tsipokor , this programming assignment is my own work
+# and i have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #Print my initials using large block letters
+    print("")
+    print("...AMT")
+    print("")
 
 
+    print(".......AAAA............MMM......MMM........TTTTTTTTTTTT.....")
+    print(".....AAA..AAA..........MMMM....MMMM.............TTT.........")
+    print("....AAA....AAA.........MMMMMMMMMMMM.............TTT.........")
+    print("...AAA......AAA........MMM..MM..MMM.............TTT.........")
+    print("...AAAAAAAAAAAA........MMM......MMM.............TTT.........")
+    print("...AAA......AAA........MMM......MMM.............TTT.........")
+    print("...AAA......AAA........MMM......MMM.............TTT.........")
+    print("...AAA......AAA........MMM......MMM.............TTT.........")
+    print("...AAA......AAA...**...MMM......MMM...**........TTT.......**")
+    print("...AAA......AAA...**...MMM......MMM...**........TTT.......**")
 
+    print("")
 main()
